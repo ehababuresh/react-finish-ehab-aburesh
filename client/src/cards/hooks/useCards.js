076fa4f9d5c83c4ba09624cdd38d9e488ee3dfd5
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2023-02-08 21:19:55 +0200

    Level 9 | axios

---
+++ client/src/cards/hooks/useCards.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { getCards } from "./../services/cardApiService";
+
+const useCards = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [cards, setCards] = useState();
+  const [card, setCard] = useState();
+
+  const requestStatus = (loading, errorMessage, cards, card = null) => {
+    setLoading(loading);
+    setError(errorMessage);
+    setCards(cards);
+    setCard(card);
+  };
+
+  const handleGetCards = async () => {
+    try {
+      setLoading(true);
+      const cards = await getCards();
+      requestStatus(false, null, cards);
+    } catch (error) {
+      requestStatus(false, error, null);
+    }
+  };
+
+  return { card, cards, isLoading, error, handleGetCards };
+};
+
+export default useCards;
--- client/src/cards/pages/CardsPage.jsx
+++ client/src/cards/pages/CardsPage.jsx
@@ -1,86 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "@mui/material/Container";
 import PageHeader from "../../components/PageHeader";
-import Cards from "../components/Cards";
+import CardsFeedback from "../components/CardsFeedback";
+import useCards from "./../hooks/useCards";
 
 const CardsPage = () => {
-  const cards = [
-    {
-      _id: "63765801e20ed868a69a62c4a",
-      title: "first",
-      subtitle: "subtitle",
-      description: "testing 123",
-      phone: "050-0000000",
-      email: "test@gmail.com",
-      web: "https://www.test.co.il",
-      image: {
-        url: "https://cdn.pixabay.com/photo/2016/04/20/08/21/entrepreneur-1340649_960_720.jpg",
-        alt: "Business card image",
-      },
-      address: {
-        state: "",
-        country: "country",
-        city: "tel aviv",
-        street: "shinkin",
-        houseNumber: 3,
-        zip: 1234,
-      },
-      bizNumber: 1_000_000,
-      user_id: "63765801e20ed868a69a62c2",
-      createdAt: new Date().toLocaleTimeString(),
-      likes: [],
-    },
-    {
-      _id: "63765801e20ed868a69a62c4b",
-      title: "second",
-      subtitle: "subtitle",
-      description: "testing 123",
-      phone: "050-0000000",
-      email: "test@gmail.com",
-      web: "https://www.test.co.il",
-      image: {
-        url: "https://cdn.pixabay.com/photo/2016/04/20/08/21/entrepreneur-1340649_960_720.jpg",
-        alt: "Business card image",
-      },
-      address: {
-        state: "",
-        country: "country",
-        city: "tel aviv",
-        street: "shinkin",
-        houseNumber: 3,
-        zip: 1234,
-      },
-      bizNumber: 1_000_001,
-      user_id: "63765801e20ed868a69a62c2",
-      createdAt: new Date().toLocaleTimeString(),
-      likes: ["63765801e20ed868a69a62c4f"],
-    },
-    {
-      _id: "63765801e20ed868a69a62c4f",
-      title: "third",
-      subtitle: "subtitle",
-      description: "testing 123",
-      phone: "050-0000000",
-      email: "test@gmail.com",
-      web: "",
-      image: {
-        url: "https://cdn.pixabay.com/photo/2016/04/20/08/21/entrepreneur-1340649_960_720.jpg",
-        alt: "Business card image",
-      },
-      address: {
-        state: "",
-        country: "country",
-        city: "tel aviv",
-        street: "shinkin",
-        houseNumber: 3,
-        zip: 1234,
-      },
-      bizNumber: 1_000_002,
-      user_id: "63765801e20ed868a69a62c2",
-      createdAt: new Date().toLocaleTimeString(),
-      likes: [],
-    },
-  ];
+  const { cards, error, isLoading, handleGetCards } = useCards();
+
+  useEffect(() => {
+    handleGetCards();
+  }, []);
 
   return (
     <Container>
@@ -88,7 +17,12 @@ const CardsPage = () => {
         title="Cards Page"
         subtitle="On this page you can find all business cards form all categories"
       />
-      <Cards cards={cards} />
+      <CardsFeedback
+        cards={cards}
+        error={error}
+        isLoading={isLoading}
+        onDelete={() => {}}
+      />
     </Container>
   );
 };
